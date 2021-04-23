31c1826461740ead1f3acc98c23441e9dabe46f2
yfaria <yuriluisfaria@gmail.com>
2021-04-23 02:44:21 -0300

    colocando o feito ate agora

--- src/chapter-6.lean
+++ src/chapter-6.lean
@@ -62,11 +62,16 @@ end Naturals
 
 namespace Lists
 
+#print list.cons_append
+
 example {a : Type} {xs ys zs : list a} : (xs ++ ys) ++ zs = xs ++ (ys ++ zs) :=
 begin
  induction xs with b hb,
  apply list.append.equations._eqn_1,
- sorry
+ rw list.cons_append,
+ rw list.cons_append,
+ rw list.cons_append,
+ rw xs_ih
 end
 
 def map {a b : Type} : (a → b) → list a → list b
@@ -169,6 +174,8 @@ def sum₁ : list ℕ → ℕ
  | []        := 0
  | (x :: xs) := x + sum₁ xs 
 
+def sum₂ : list ℕ → ℕ  := foldr (+) 0
+
 def concat {a : Type} : list (list a) → list a 
  | []          := []
  | (xs :: xss) := xs ++ concat xss 
@@ -188,20 +195,68 @@ def filter {a : Type} : (a → bool) → list a → list a
 #reduce filter (λ x, if x < 4 then tt else ff) [1,2,3,4]
 
 
-theorem sum'_eq_sum'' {xs : list ℕ} : sum₁ xs = sum₂ xs := sorry
+
+theorem sum'_eq_sum'' {xs : list ℕ} : sum₁ xs = sum₂ xs := 
+begin
+ induction xs with x xs h,
+ rw sum₁,
+ rw sum₂,
+ rw foldr,
+
+ rw sum₁,
+ rw sum₂,
+ rw foldr,
+ rw← sum₂,
+ rw h,
+end
 
 
-theorem sum_of_append {xs ys : list ℕ} : sum₁ (xs ++ ys) = sum₁ xs + sum₁ ys := sorry
+theorem sum_of_append {xs ys : list ℕ} : sum₁ (xs ++ ys) = sum₁ xs + sum₁ ys := 
+begin
+ induction xs with x xs h,
+ rw sum₁,
+ -- apply list.append.equations._eqn_1,
+ dsimp,
+ rw nat.zero_add,
+ 
+ rw sum₁,
+ dsimp,
+ rw sum₁,
+ rw h,
+ rw nat.add_assoc
+end
 
 
 theorem concat_of_apend { a : Type} {xss yss : list (list a)} : 
-  concat (xss ++ yss) = concat xss ++ concat yss := sorry
+  concat (xss ++ yss) = concat xss ++ concat yss :=
+begin
+ induction xss with xs xss hx,
+ rw concat,
+ simp,
+   
+ rw concat,
+ simp,
+ rw concat,
+ rw hx
+end
 
 
 theorem foldr_law {a b : Type} (f : a → b → b) (g : b → b → b) (e : b) (xs ys : list a) 
   (h1 : ∀ x, g e x = x) 
   (h2 : ∀ x y z, f x (g y z) = g (f x y) z) : 
-  foldr f e (xs ++ ys) = g (foldr f e xs) (foldr f e ys) := sorry
+  foldr f e (xs ++ ys) = g (foldr f e xs) (foldr f e ys) :=
+begin
+ induction xs with x xs hx,
+ rw foldr,
+ dsimp,
+ rw h1,
+
+ rw foldr,
+ dsimp,
+ rw foldr,
+ rw← h2,
+ rw hx
+end
 
 end Foldr
 
@@ -222,7 +277,6 @@ def concat {a : Type} := foldr (++) ([] : list a)
 
 def double (n : ℕ) := n + n
 
-
 theorem funsion_law {a b : Type} (f : a → b) (g : b → a → a) (h : b → b → b) 
   (xa : a) (xb : b)
   (h1 : f xa = xb) 
@@ -232,20 +286,115 @@ begin
  funext xs,
  induction xs with d hd, 
  rw foldr, rw comp, dsimp, rw foldr, exact h1, 
- sorry
+ 
+ rw comp, 
+ dsimp,
+ rw foldr,
+ rw foldr,
+ rw h2,
+ rw comp at xs_ih,
+ dsimp at xs_ih,
+ rw xs_ih
+
 end
 
+
+lemma funsion2 {α β : Type} (a : α) 
+ (f :  β → α → α) (g : α → β)
+ : foldr f a ∘ map₁ g = foldr (f ∘ g) a :=
+begin
+ rw map₁,
+ dsimp,
+ rw funsion_law,
+ rw foldr,
+ intro x,
+ intro ys,
+ rw comp,
+ dsimp,
+ rw foldr
+end
+
+/-
+Relendo, acho que isso até faz sentido mas queríamos o resultado acima.
 
 lemma funsion1 {α β : Type} (a : α) 
  (f :  β → α → α) (h : α → α → α) (g : α → β) 
  (h1 : foldr f a [] = a)
  (h2 : ∀ x y, foldr f a (((::) ∘ g) x y) = h x (foldr f a y))
- : foldr f a ∘ map₁ g = foldr h a := sorry
+ : foldr f a ∘ map₁ g = foldr h a :=
+begin
+ rw map₁,
+ dsimp,
+ rw funsion_law,
+ exact h1,
+ exact h2,
+end
+-/
 
+example {a : Type} (xs : list ℕ) : (double ∘ sum₂) xs = foldr ((+) ∘ double) 0 xs := 
+begin
+ rw sum₂,
+ rw funsion_law,
+ rw double,
+ rw comp,
+ dsimp,
+ intro x,
+ intro y,
+ rw double,
+ rw double,
+ rw double,
+ ring -- comutatividade e associatividade da soma.
+end
 
-example {a : Type} (xs : list ℕ) : (double ∘ sum₂) xs = foldr ((+) ∘ double) 0 xs := sorry
+/-
+O livro fala pra usar a fusion law aqui, mas não é tão simples.
+Para a lei precisaríamos de colocar f = length, g = (++) e h = ((+) ∘ length),
+mas quando formulamos nossa restrição é f: a → b - então a seria o nosso list ℕ e b seria ℕ - e g : b → a → a , e essa tipagem já quebra o que queríamos fazer pois a função append recebe duas listas de um mesmo tipo.
+example (xs : list ℕ) : (length ∘ concat) = foldr ((+) ∘ length) 0 := 
+begin
+ rw concat,
+ rw funsion_law length (++) ,
+ sorry
+end
+-/
 
-example {a : Type} (xs : list a) : (length ∘ concat) = foldr ((+) ∘ length) 0 := sorry
+lemma sum_lengths (xs ys : list ℕ): 
+length (xs ++ ys) = length xs + length ys :=
+begin
+ rw length, dsimp,
+ induction xs with x xs h,
+ rw foldr,
+ dsimp,
+ rw zero_add,
+ 
+ rw foldr,
+ rw list.cons_append,
+ rw foldr,
+ rw h,
+ ring
+end
+
+example (xs : list ℕ) : (length ∘ concat) = foldr ((+) ∘ length) 0 := 
+begin
+ rw concat,
+ funext xss,
+ induction xss with xs xss h,
+ 
+ rw comp,
+ dsimp,
+ rw comp,
+ rw foldr,
+ rw length,
+ rw foldr,
+ rw foldr,
+
+ rw comp, dsimp,
+ rw foldr,
+ rw foldr,
+ rw← h,
+ rw comp, dsimp,
+ rw sum_lengths
+end
 
 
 end Fusion
@@ -269,8 +418,31 @@ def reverse₂ {a : Type} := foldl (flip (::)) ([] : list a)
 
 #reduce reverse₁ [1,2,3,4]
 
+lemma rev2_eq : ∀ x: ℕ, ∀ xs : list ℕ , reverse₂ xs ++ [x] = reverse₂ (x :: xs) :=
+begin
+ rw reverse₂, dsimp,
+ intro x, intro xs,
+ rw foldl, rw flip,
+ induction xs with y ys h,
+ rw foldl, rw foldl, dsimp, refl,
+ rw foldl, rw foldl, rw flip, rw flip,
+ sorry
+end
 
-example : ∀ xs : list ℕ, reverse₁ xs = reverse₂ xs := sorry
+example : ∀ xs : list ℕ, reverse₁ xs = reverse₂ xs := 
+begin
+ intro xs,
+ induction xs with x xs h,
+ rw reverse₂, dsimp, rw reverse₁, rw foldl,
+
+ rw reverse₁,
+ rw h,
+ rw rev2_eq, -- falta provar.
+ -- rw foldl,
+ -- rw flip,
+ -- rw h,
+ -- sorry
+end
 
 end Foldl
 
@@ -316,10 +488,25 @@ def tails {a : Type} : list a → list (list a)
  | []  := [[]]
  | (x :: xs) := (x::xs) :: tails xs
 
+def foldr {a b : Type} : (a → b → b) → b → list a → b 
+| f e [] := e
+| f e (x :: xs) := f x (foldr f e xs)
 
-def segments {a : Type} : list a := concat ∘ map inits ∘ tails
+def map {a b : Type} (g : a → b) : list a → list b := foldr ((::) ∘ g) []
 
-def mss₁ := maximum ∘ map sum ∘ segments
+def concat {a : Type} := foldr (++) ([] : list a)
+
+def inits {a : Type} : list a → list (list a)
+| [] := [[]]
+| (x :: xs) := [] :: (map ((::) x) (inits xs))
+
+def segments: list ℕ → list (list ℕ) := concat ∘ map inits ∘ tails
+
+#check map list.sum ∘ segments
+
+#check maximum ∘ map list.sum ∘ segments
+
+def mss₁ (xs : list ℕ) : list ℕ → ℕ := maximum $ map list.sum $ segments xs
 
 
 def mss₂ := 
